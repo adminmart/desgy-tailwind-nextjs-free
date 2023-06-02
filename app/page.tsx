5a3ffcf543226e0cd1118cdf33812adb2892fb31
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2023-06-02 12:12:43 +0530

    updated

---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+import Banner from './components/Banner/index';
+import Aboutus from './components/Aboutus/index';
+import Dedicated from './components/Dedicated/index';
+import Digital from './components/Digital/index';
+import Beliefs from './components/Beliefs/index';
+import Wework from './components/Wework/index';
+import Ourteam from './components/Ourteam/index';
+import Featured from './components/Featured/index';
+import Manage from './components/Manage/index';
+import FAQ from './components/FAQ/index';
+import Testimonials from './components/Testimonials/index';
+import Articles from './components/Articles/index';
+import Joinus from './components/Joinus/index';
+import Insta from './components/Insta/index';
+
+
+export default function Home() {
+  return (
+    <main>
+      <Banner />
+      <Aboutus />
+      <Dedicated />
+      <Digital />
+      <Beliefs />
+      <Wework />
+      <Ourteam />
+      <Featured />
+      <Manage />
+      <FAQ />
+      <Testimonials />
+      <Articles />
+      <Joinus />
+      <Insta />
+    </main>
+  )
+}
